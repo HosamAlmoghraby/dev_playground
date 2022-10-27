3bdb0a7010d2065a1051d37d3a5663ad02431f0f
Hosam <hosam@gmail.com>
2022-10-27 00:42:10 +0000

    test

--- blueprint/blueprint/settings.py
+++ blueprint/blueprint/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8)vaau5s3#$z^lq48j$(zref0c%o=ta4n8p*uzs&8^f8%yu_n3"
+SECRET_KEY = "django-insecure-8)vaaud5s3#$z^lq48j$(zref0c%o=ta4n8p*uzs&8^f8%yu_n3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
